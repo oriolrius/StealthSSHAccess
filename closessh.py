5e019dfaa30b594fbefc8197bf706c38497057bc
Oriol Rius <oriol@oriolrius.cat>
2023-08-08 20:56:20 +0200

    it seems that it works

--- closessh.py
+++ closessh.py
@@ -17,6 +17,12 @@ logger.debug(f'LOGLEVEL = {LOGLEVEL}')
 # Configuration
 TIMEOUT = int(os.getenv('TIMEOUT')) or 600 # Timeout in seconds
 WAIT_LOOP = int(os.getenv('TIMEOUT')) or 60 # 1'
+iface = os.getenv('IFACE') or "eth0"
+iface_ip = os.getenv("IFACE_IP") or "172.19.0.2"
+port_to_monitor = int(os.getenv('PORT_TO_MONITOR')) or 55888
+filter_expr = f"tcp port {port_to_monitor}"
+port_to_open = int(os.getenv('PORT_TO_OPEN')) or 55222
+PICKLE_FILE = '/data/' + os.getenv('PICKLE_FILE') or 'timers.pkl'
 
 # Program data
 ip_timers = {}
@@ -24,6 +30,7 @@ ip_timers = {}
 def check_and_close_ports(ip):
     # Check if there is an active connection from this IP
     for connection in psutil.net_connections(kind='inet'):
+        # logger.debug(f'connection: {connection}')
         comparison_results = (
             connection.laddr.port == port_to_open,
             connection.status == 'ESTABLISHED',
@@ -33,8 +40,10 @@ def check_and_close_ports(ip):
             logger.info(f'SSH connection detected from triggering IP {ip}, resetting timer to check again later')
             ip_timers[ip] = time.time()
             return
+
     # Check if it's unsed for more than TIMEOUT
     ttl = time.time() - ip_timers[ip]
+    logger.debug(f"Calculating Time-to-live (TTL) for IP {ip}. TTL: {ttl}, TIMEOUT: {TIMEOUT}.")
     if ttl > TIMEOUT:
         close_port(ip)
 
@@ -50,7 +59,9 @@ def close_port(ip):
 
 if __name__ == "__main__":
     while True:
-        load_timers()
-        for ip in ip_timers:
+        ip_timers = load_timers()
+        logger.info(ip_timers)
+        for ip in dict(ip_timers): # make a copy for iterating, avoid problems removing items of the dict
+            logger.debug(f'check_and_close_ports: {ip}')
             check_and_close_ports(ip)
         time.sleep(WAIT_LOOP) # Sleep for a short time to prevent busy-waiting
--- openssh.py
+++ openssh.py
@@ -5,7 +5,6 @@ import psutil
 from scapy.all import *
 import time
 import pickle
-from closessh import 
 
 # Debug
 LOGLEVEL = os.environ.get('LOGLEVEL', 'INFO').upper()
@@ -21,30 +20,35 @@ iface_ip = os.getenv("IFACE_IP") or "172.19.0.2"
 port_to_monitor = int(os.getenv('PORT_TO_MONITOR')) or 55888
 filter_expr = f"tcp port {port_to_monitor}"
 port_to_open = int(os.getenv('PORT_TO_OPEN')) or 55222
-PICKLE_FILE = os.getenv('PICKLE_FILE')) or 'timers.pkl'
+PICKLE_FILE = '/data/' + os.getenv('PICKLE_FILE') or 'timers.pkl'
 
 # Program data
 ip_timers = {} # This will store timers for each triggering IP
 
 def load_timers():
     # Load pickled data from file
-    with open(PICKLE_FILE, 'rb') as file:
-        ip_timers = pickle.load(file)
+    if os.path.exists(PICKLE_FILE):
+        with open(PICKLE_FILE, 'rb') as file:
+            ip_timers = pickle.load(file)
+    else:
+        update_timers()
     logger.info(f"Loaded pickle file: {PICKLE_FILE} - ip_timers: {ip_timers}")
+    return ip_timers
 
 def update_timers():
     with open(PICKLE_FILE, 'wb') as file:
         pickle.dump(ip_timers, file)
     logger.info(f"Updated pickle file: {PICKLE_FILE} - ip_timers: {ip_timers}")
+    
+    return ip_timers
 
 def open_port(ip):
     # Open the port for the given IP if it's not already open
     if os.system(f'/sbin/iptables -C INPUT -i {iface} -d {iface_ip} -p tcp --dport {port_to_open} -s {ip} -j ACCEPT') == 0:
-        return
-
-    os.system(f'/sbin/iptables -I INPUT -i {iface} -d {iface_ip} -p tcp --dport {port_to_open} -s {ip} -j ACCEPT')
-    logger.info(f'Port {port_to_open} opened for IP: {ip}')
-
+        logger.debug(f'Port: {port} already open for source IP address: {ip}')
+    else:
+        os.system(f'/sbin/iptables -I INPUT -i {iface} -d {iface_ip} -p tcp --dport {port_to_open} -s {ip} -j ACCEPT')
+        logger.info(f'Port {port_to_open} opened for IP: {ip}')
     # Set the timer for this IP
     ip_timers[ip] = time.time()
     update_timers()
@@ -59,13 +63,15 @@ def ensure_drop_rules(port):
     drop_rule = f'/sbin/iptables -C INPUT -i {iface} -d {iface_ip} -p tcp --dport {port} -j DROP'
     if os.system(drop_rule) != 0:
         os.system(f'/sbin/iptables -I INPUT -i {iface} -d {iface_ip} -p tcp --dport {port} -j DROP')
-        logger.info('DROP rule added for port {port_to_open}')
+        logger.info(f'DROP rule added for port {port}')
+    else:
+        logger.info(f'DROP rule already existing for port {port}')
 
 if __name__ == "__main__":
     # Ensure default behaviour
     ensure_drop_rules(port_to_monitor)
     ensure_drop_rules(port_to_open)
     # load old timers
-    load_timers()
+    ip_timers = load_timers()
     # Capturing traffic
     sniff(filter=filter_expr, iface=iface, prn=process_packet, store=0)
