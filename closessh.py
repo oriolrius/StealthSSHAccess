577abf8e9df138a5c4d9e415600cff33881d12f2
Nestor <nestor@nstlopez.com>
2023-10-19 20:12:13 +0200

    chore: format with black

--- closessh.py
+++ closessh.py
@@ -7,62 +7,75 @@ import time
 from openssh import update_timers, load_timers, run_cmd
 
 # Debug
-LOGLEVEL = os.environ.get('LOGLEVEL', 'INFO').upper()
-FORMAT = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+LOGLEVEL = os.environ.get("LOGLEVEL", "INFO").upper()
+FORMAT = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
 logging.root.setLevel(logging.DEBUG)
 logging.basicConfig(level=LOGLEVEL, format=FORMAT, handlers=[logging.StreamHandler()])
 logger = logging.getLogger("closessh")
-logger.debug(f'LOGLEVEL = {LOGLEVEL}')
+logger.debug(f"LOGLEVEL = {LOGLEVEL}")
 
 # Configuration
-TIMEOUT = int(os.getenv('TIMEOUT')) or 600 # Timeout in seconds
-WAIT_LOOP = int(os.getenv('TIMEOUT')) or 60 # 1'
-iface = os.getenv('IFACE') or "eth0"
+TIMEOUT = int(os.getenv("TIMEOUT")) or 600  # Timeout in seconds
+WAIT_LOOP = int(os.getenv("TIMEOUT")) or 60  # 1'
+iface = os.getenv("IFACE") or "eth0"
 iface_ip = os.getenv("IFACE_IP") or "172.19.0.2"
-port_to_monitor = int(os.getenv('PORT_TO_MONITOR')) or 55888
+port_to_monitor = int(os.getenv("PORT_TO_MONITOR")) or 55888
 filter_expr = f"tcp port {port_to_monitor}"
-port_to_open = os.getenv('PORT_TO_OPEN') or 55222
+port_to_open = os.getenv("PORT_TO_OPEN") or 55222
 port_to_open = int(port_to_open)
-PICKLE_FILE = '/data/' + ( os.getenv('PICKLE_FILE') or 'timers.pkl')
+PICKLE_FILE = "/data/" + (os.getenv("PICKLE_FILE") or "timers.pkl")
 
 # Program data
 ip_timers = {}
 
+
 def check_and_close_ports(ip):
     # Check if there is an active connection from this IP
-    for connection in psutil.net_connections(kind='inet'):
+    for connection in psutil.net_connections(kind="inet"):
         # logger.debug(f'connection: {connection}')
         comparison_results = (
             connection.laddr.port == port_to_open,
-            connection.status == 'ESTABLISHED',
-            connection.raddr and connection.raddr[0] == ip  # check if raddr is not empty and access the IP address from the tuple
+            connection.status == "ESTABLISHED",
+            connection.raddr
+            and connection.raddr[0]
+            == ip,  # check if raddr is not empty and access the IP address from the tuple
         )
         if all(comparison_results):
-            logger.info(f'SSH connection detected from triggering IP {ip}, resetting timer to check again later')
+            logger.info(
+                f"SSH connection detected from triggering IP {ip}, resetting timer to check again later"
+            )
             ip_timers[ip] = time.time()
             return
 
     # Check if it's unsed for more than TIMEOUT
     ttl = time.time() - ip_timers[ip]
-    logger.debug(f"Calculating Time-to-live (TTL) for IP {ip}. TTL: {ttl}, TIMEOUT: {TIMEOUT}.")
+    logger.debug(
+        f"Calculating Time-to-live (TTL) for IP {ip}. TTL: {ttl}, TIMEOUT: {TIMEOUT}."
+    )
     if ttl > TIMEOUT:
         close_port(ip)
 
+
 def close_port(ip):
     # If no active connections are found, remove the ACCEPT entry for this IP
-    run_cmd(f'/sbin/iptables -t mangle -D PREROUTING -i {iface} -d {iface_ip} -p tcp --dport {port_to_open} -s {ip} -j ACCEPT')
-    logger.info(f'Removed ACCEPT rule for IP: {ip}')
+    run_cmd(
+        f"/sbin/iptables -t mangle -D PREROUTING -i {iface} -d {iface_ip} -p tcp --dport {port_to_open} -s {ip} -j ACCEPT"
+    )
+    logger.info(f"Removed ACCEPT rule for IP: {ip}")
     try:
         del ip_timers[ip]
         update_timers()
     except KeyError:
         pass
 
+
 if __name__ == "__main__":
     while True:
         ip_timers = load_timers()
         logger.info(ip_timers)
-        for ip in dict(ip_timers): # make a copy for iterating, avoid problems removing items of the dict
-            logger.debug(f'check_and_close_ports: {ip}')
+        for ip in dict(
+            ip_timers
+        ):  # make a copy for iterating, avoid problems removing items of the dict
+            logger.debug(f"check_and_close_ports: {ip}")
             check_and_close_ports(ip)
-        time.sleep(WAIT_LOOP) # Sleep for a short time to prevent busy-waiting
+        time.sleep(WAIT_LOOP)  # Sleep for a short time to prevent busy-waiting
--- openssh.py
+++ openssh.py
@@ -8,81 +8,104 @@ import pickle
 import subprocess
 
 # Debug
-LOGLEVEL = os.environ.get('LOGLEVEL', 'INFO').upper()
-FORMAT = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+LOGLEVEL = os.environ.get("LOGLEVEL", "INFO").upper()
+FORMAT = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
 logging.root.setLevel(logging.DEBUG)
 logging.basicConfig(level=LOGLEVEL, format=FORMAT, handlers=[logging.StreamHandler()])
 logger = logging.getLogger("openssh")
-logger.debug(f'LOGLEVEL = {LOGLEVEL}')
+logger.debug(f"LOGLEVEL = {LOGLEVEL}")
 
 # Configuration
-iface = os.getenv('IFACE') or "eth0"
+iface = os.getenv("IFACE") or "eth0"
 iface_ip = os.getenv("IFACE_IP") or "172.19.0.2"
-port_to_monitor = os.getenv('PORT_TO_MONITOR') or 55888
+port_to_monitor = os.getenv("PORT_TO_MONITOR") or 55888
 port_to_monitor = int(port_to_monitor)
 filter_expr = f"tcp port {port_to_monitor}"
-port_to_open = os.getenv('PORT_TO_OPEN') or 55222
+port_to_open = os.getenv("PORT_TO_OPEN") or 55222
 port_to_open = int(port_to_open)
-PICKLE_FILE = '/data/' + ( os.getenv('PICKLE_FILE') or 'timers.pkl')
+PICKLE_FILE = "/data/" + (os.getenv("PICKLE_FILE") or "timers.pkl")
 
 # Program data
-ip_timers = {} # This will store the timers for each triggering IP
+ip_timers = {}  # This will store the timers for each triggering IP
+
 
 def load_timers():
     # Load pickled data from file
     if os.path.exists(PICKLE_FILE):
-      try:
-        with open(PICKLE_FILE, 'rb') as file:
-            ip_timers = pickle.load(file)
-      except pickle.UnpicklingError as e:
-        logger.debug(f"Error loading pickle file: {PICKLE_FILE} - {e}")
-        update_timers()
+        try:
+            with open(PICKLE_FILE, "rb") as file:
+                ip_timers = pickle.load(file)
+        except pickle.UnpicklingError as e:
+            logger.debug(f"Error loading pickle file: {PICKLE_FILE} - {e}")
+            update_timers()
     else:
         update_timers()
     logger.info(f"Loaded pickle file: {PICKLE_FILE} - ip_timers: {ip_timers}")
     return ip_timers
 
+
 def update_timers():
-    with open(PICKLE_FILE, 'wb') as file:
+    with open(PICKLE_FILE, "wb") as file:
         pickle.dump(ip_timers, file)
     logger.info(f"Updated pickle file: {PICKLE_FILE} - ip_timers: {ip_timers}")
-    
+
     return ip_timers
+
 
 def open_port(ip):
     # Open the port for the given IP if it's not already open
-    if run_cmd(f'/sbin/iptables -t mangle -C PREROUTING -i {iface} -d {iface_ip} -p tcp --dport {port_to_open} -s {ip} -j ACCEPT') == 0:
-        logger.debug(f'Port: {port_to_open} already open for source IP address: {ip}')
+    if (
+        run_cmd(
+            f"/sbin/iptables -t mangle -C PREROUTING -i {iface} -d {iface_ip} -p tcp --dport {port_to_open} -s {ip} -j ACCEPT"
+        )
+        == 0
+    ):
+        logger.debug(f"Port: {port_to_open} already open for source IP address: {ip}")
     else:
-        run_cmd(f'/sbin/iptables -t mangle -I PREROUTING -i {iface} -d {iface_ip} -p tcp --dport {port_to_open} -s {ip} -j ACCEPT')
-        logger.info(f'Port {port_to_open} opened for IP: {ip}')
+        run_cmd(
+            f"/sbin/iptables -t mangle -I PREROUTING -i {iface} -d {iface_ip} -p tcp --dport {port_to_open} -s {ip} -j ACCEPT"
+        )
+        logger.info(f"Port {port_to_open} opened for IP: {ip}")
     # Set the timer for this IP
     ip_timers[ip] = time.time()
     update_timers()
 
+
 def process_packet(packet):
-    if packet.haslayer(TCP) and (packet[TCP].flags == 'S'):
+    if packet.haslayer(TCP) and (packet[TCP].flags == "S"):
         src_ip = packet[IP].src
         open_port(src_ip)
 
+
 def ensure_drop_rules(port):
     # Check if the DROP rule for port {port_to_open} exists, and if not, add it
-    if  run_cmd(f'/sbin/iptables -t mangle -C PREROUTING -i {iface} -d {iface_ip} -p tcp --dport {port} -j DROP') != 0:
-        run_cmd(f'/sbin/iptables -t mangle -I PREROUTING -i {iface} -d {iface_ip} -p tcp --dport {port} -j DROP')
-        logger.info(f'DROP rule added for port {port}')
+    if (
+        run_cmd(
+            f"/sbin/iptables -t mangle -C PREROUTING -i {iface} -d {iface_ip} -p tcp --dport {port} -j DROP"
+        )
+        != 0
+    ):
+        run_cmd(
+            f"/sbin/iptables -t mangle -I PREROUTING -i {iface} -d {iface_ip} -p tcp --dport {port} -j DROP"
+        )
+        logger.info(f"DROP rule added for port {port}")
     else:
-        logger.info(f'DROP rule already existing for port {port}')
+        logger.info(f"DROP rule already existing for port {port}")
+
 
 def run_cmd(cmd):
-    logger.debug(f'Running command: {cmd}')
-    #exit_code = subprocess.run(cmd.split(' '), check=True)
+    logger.debug(f"Running command: {cmd}")
+    # exit_code = subprocess.run(cmd.split(' '), check=True)
     try:
-        subprocess.run(cmd.split(' '), stdout=subprocess.PIPE, stderr=subprocess.PIPE, check=True)
+        subprocess.run(
+            cmd.split(" "), stdout=subprocess.PIPE, stderr=subprocess.PIPE, check=True
+        )
         exit_code = 0  # Rule exists
     except subprocess.CalledProcessError:
         exit_code = 1  # Rule doesn't exist
-    logger.debug(f'Exit code: {exit_code}')
+    logger.debug(f"Exit code: {exit_code}")
     return exit_code
+
 
 if __name__ == "__main__":
     # Ensure default behaviour
