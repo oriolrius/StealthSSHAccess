9d438f2f09b4aca0b9cc28fe35986ac9417c1c46
Oriol Rius <oriol@joor.net>
2023-12-21 18:12:33 +0100

    before creating library code

--- closessh.py
+++ closessh.py
@@ -1,84 +1,116 @@
+"""
+Close SSH Ports Module
+
+This module periodically checks and closes open SSH ports that are no longer in use. 
+It's designed to work in conjunction with the openssh.py script, which opens ports based on certain 
+network conditions. 
+This script uses the iptables command to manage port rules and psutil for inspecting active network 
+connections.
+
+The script's behavior can be configured through environment variables, allowing the specification of 
+network interfaces, IP addresses, and ports to monitor. 
+It also handles logging and updating the timer data using functions imported from the openssh 
+module.
+
+Functions:
+- check_and_close_ports: Checks for active connections on open ports and closes them if inactive.
+- close_port: Closes an open port for a specific IP address.
+
+Usage:
+The script runs in an infinite loop, periodically checking for ports to close. 
+Ensure that the required environment variables are set before running.
+
+Example:
+    $ python closessh.py
+"""
+
 import os
 import logging
+import time
 import psutil
-import time
-import time
 from openssh import update_timers, load_timers, run_cmd
 
 # Debug
 LOGLEVEL = os.environ.get("LOGLEVEL", "INFO").upper()
 FORMAT = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
-logging.root.setLevel(logging.DEBUG)
 logging.basicConfig(level=LOGLEVEL, format=FORMAT, handlers=[logging.StreamHandler()])
 logger = logging.getLogger("closessh")
 logger.debug(f"LOGLEVEL = {LOGLEVEL}")
 
 # Configuration
-TIMEOUT = int(os.getenv("TIMEOUT")) if os.getenv("TIMEOUT") is not None else 600
-WAIT_LOOP = int(os.getenv("WAIT_LOOP")) if os.getenv("WAIT_LOOP") is not None else 60
-
-iface = os.getenv("IFACE") or "eth0"
-iface_ip = os.getenv("IFACE_IP") or "172.19.0.2"
-port_to_monitor = [
-    int(port) for port in (os.getenv("PORT_TO_MONITOR") or "55888").split(",")
-]
-port_to_open = [int(port) for port in (os.getenv("PORT_TO_OPEN") or "55222").split(",")]
-filter_expr = f"tcp port {' or tcp port '.join(str(port) for port in port_to_monitor)}"
-
-PICKLE_FILE = "/data/" + (os.getenv("PICKLE_FILE") or "timers.pkl")
+TIMEOUT = int(os.getenv("TIMEOUT", 600))
+WAIT_LOOP = int(os.getenv("WAIT_LOOP", 60))
+iface = os.getenv("IFACE", "eth0")
+iface_ip = os.getenv("IFACE_IP", "172.19.0.2")
+port_to_monitor = int(os.getenv("PORT_TO_MONITOR", 55888))
+ports_to_open = [int(port) for port in os.getenv("PORTS_TO_OPEN", "55222").split(",")]
+PICKLE_FILE = "/data/" + os.getenv("PICKLE_FILE", "timers.pkl")
 
 # Program data
 ip_timers = {}
 
-
 def check_and_close_ports(ip):
-    for port in port_to_open:
+    """
+    Checks each open port for a given IP address and closes it if inactive.
+
+    This function iterates through all ports in `ports_to_open` and checks for any
+    active connections from the given IP address. If a port has been inactive for
+    longer than the specified TIMEOUT, it triggers the port to close.
+
+    Parameters:
+    ip (str): The IP address for which to check open ports.
+    """
+    for port in ports_to_open:
         # Check if there is an active connection from this IP
         for connection in psutil.net_connections(kind="inet"):
-            # logger.debug(f'connection: {connection}')
             comparison_results = (
                 connection.laddr.port == port,
                 connection.status == "ESTABLISHED",
-                connection.raddr
-                and connection.raddr[0]
-                == ip,  # check if raddr is not empty and access the IP address from the tuple
+                connection.raddr and connection.raddr[0] == ip
             )
             if all(comparison_results):
                 logger.info(
-                    f"SSH connection detected from triggering IP {ip}, resetting timer to check again later"
+                  "SSH connection detected from IP {}, resetting timer "
+                  "to check again later".format(ip)
                 )
                 ip_timers[ip] = time.time()
+                return
 
-        # Check if it's unsed for more than TIMEOUT
-        ttl = time.time() - ip_timers[ip]
-        logger.debug(
-            f"Calculating Time-to-live (TTL) for IP {ip}. TTL: {ttl}, TIMEOUT: {TIMEOUT}."
-        )
+        # Check if it's unused for more than TIMEOUT
+        ttl = time.time() - ip_timers.get(ip, 0)
+        logger.debug(f"Calculating Time-to-live (TTL) for IP {ip}. TTL: {ttl}, TIMEOUT: {TIMEOUT}.")
         if ttl > TIMEOUT:
             close_port(ip, port)
 
-
 def close_port(ip, port):
-    # If no active connections are found, remove the ACCEPT entry for this IP
-    run_cmd(
-        f"/sbin/iptables -t mangle -D PREROUTING -i {iface} -d {iface_ip} -p tcp --dport {port} -s {ip} -j ACCEPT"
-    )
+    """
+    Closes a specified port for a given IP address.
 
-    logger.info(f"Removed ACCEPT rule for IP: {ip}")
+    This function removes the ACCEPT rule from iptables for a specific IP and port
+    combination, effectively closing the port for that IP address. It updates the
+    `ip_timers` to reflect this change.
+
+    Parameters:
+    ip (str): The IP address for which the port needs to be closed.
+    port (int): The port number to be closed for the given IP address.
+    """
+    iptables_cmd = (
+        f"/sbin/iptables -t mangle -D PREROUTING -i {iface} -d {iface_ip} "
+        f"-p tcp --dport {port} -s {ip} -j ACCEPT"
+    )
+    run_cmd(iptables_cmd)
+    logger.info(f"Removed ACCEPT rule for IP: {ip} and port: {port}")
     try:
         del ip_timers[ip]
         update_timers()
     except KeyError:
         pass
 
-
 if __name__ == "__main__":
     while True:
         ip_timers = load_timers()
-        logger.info(ip_timers)
-        for ip in dict(
-            ip_timers
-        ):  # make a copy for iterating, avoid problems removing items of the dict
-            logger.debug(f"check_and_close_ports: {ip}")
+        logger.info(f"Current IP Timers: {ip_timers}")
+        for ip in list(ip_timers):
+            logger.debug(f"Checking and potentially closing ports for IP: {ip}")
             check_and_close_ports(ip)
-        time.sleep(WAIT_LOOP)  # Sleep for a short time to prevent busy-waiting
+        time.sleep(WAIT_LOOP)  # Sleep to prevent busy-waiting
--- openssh.py
+++ openssh.py
@@ -1,15 +1,46 @@
+"""
+Network Port Management and Packet Sniffing
+
+This module provides functionality for monitoring and managing network traffic,
+specifically targeting the opening and closing of ports based on certain network
+conditions. It uses iptables for managing port rules and Scapy for sniffing network
+packets. The script monitors specified ports, opens them for certain IP addresses
+based on packet data, and maintains a record of these actions.
+
+The script's behavior can be configured through environment variables, allowing
+the specification of network interfaces, IP addresses, and ports to monitor. It
+also handles logging and persistence of port timer data using a pickle file.
+
+Functions:
+- load_timers: Loads timer data from a pickle file.
+- update_timers: Updates and saves the timer data to a pickle file.
+- open_port: Opens a port for a specific IP address.
+- process_packet: Processes each sniffed packet and takes action based on its content.
+- ensure_drop_rules: Ensures iptables DROP rules are set for specified ports.
+- run_cmd: Executes a system command.
+
+Global Variables:
+- LOGLEVEL, iface, iface_ip, port_to_monitor, ports_to_open, filter_expr, PICKLE_FILE, ip_timers
+
+Usage:
+The script can be executed as a standalone Python script. Ensure that the required
+environment variables are set before running.
+
+Example:
+    $ python this_script.py
+
+This module requires external libraries: Scapy, os, logging, time, pickle, subprocess.
+"""
 import os
 import logging
-import psutil
-from scapy.all import *
 import time
 import pickle
 import subprocess
+from scapy.all import sniff, TCP, IP
 
 # Debug
 LOGLEVEL = os.environ.get("LOGLEVEL", "INFO").upper()
 FORMAT = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
-logging.root.setLevel(logging.DEBUG)
 logging.basicConfig(level=LOGLEVEL, format=FORMAT, handlers=[logging.StreamHandler()])
 logger = logging.getLogger("openssh")
 logger.debug(f"LOGLEVEL = {LOGLEVEL}")
@@ -17,19 +48,36 @@ logger.debug(f"LOGLEVEL = {LOGLEVEL}")
 # Configuration
 iface = os.getenv("IFACE") or "eth0"
 iface_ip = os.getenv("IFACE_IP") or "172.19.0.2"
-# Configuration
-port_to_monitor = [
-    int(port) for port in (os.getenv("PORT_TO_MONITOR") or "55888").split(",")
-]
-port_to_open = [int(port) for port in (os.getenv("PORT_TO_OPEN") or "55222").split(",")]
-filter_expr = f"tcp port {' or tcp port '.join(str(port) for port in port_to_monitor)}"
+port_to_monitor = int(os.getenv("PORT_TO_MONITOR", "55888"))
+ports_to_open = [int(port) for port in os.getenv("PORTS_TO_OPEN", "55222").split(",")]
+filter_expr = f"tcp port {port_to_monitor}"
 PICKLE_FILE = "/data/" + (os.getenv("PICKLE_FILE") or "timers.pkl")
+
+# Program data
+ip_timers = {}  # This will store the timers for each triggering IP
 
 # Program data
 ip_timers = {}  # This will store the timers for each triggering IP
 
 
 def load_timers():
+    """
+    Load timer data from a pickle file.
+
+    This function attempts to load pickled data from a specified pickle file.
+    If the file exists and can be unpickled, it updates the `ip_timers` with the loaded data.
+    If the file cannot be unpickled or does not exist, it calls `update_timers` to update 
+    `ip_timers` with default or new data. After loading or updating, it logs the current state 
+    of `ip_timers`.
+
+    Returns:
+        dict: The updated `ip_timers` dictionary containing the timer data.
+
+    Side Effects:
+        - Reads from a file specified by the global `PICKLE_FILE`.
+        - Updates the global `ip_timers` dictionary.
+        - Logs information and debug messages.
+    """
     # Load pickled data from file
     if os.path.exists(PICKLE_FILE):
         try:
@@ -45,6 +93,21 @@ def load_timers():
 
 
 def update_timers():
+    """
+    Update and save the timer data to a pickle file.
+
+    This function serializes the current state of the `ip_timers` dictionary and saves it
+    to a pickle file specified by the global variable `PICKLE_FILE`. After saving the data, 
+    it logs the updated state of `ip_timers`.
+
+    Returns:
+        dict: The `ip_timers` dictionary containing the updated timer data.
+
+    Side Effects:
+        - Writes to a file specified by the global `PICKLE_FILE`.
+        - Serializes the global `ip_timers` dictionary.
+        - Logs information messages.
+    """
     with open(PICKLE_FILE, "wb") as file:
         pickle.dump(dict(ip_timers), file)
     logger.info(f"Updated pickle file: {PICKLE_FILE} - ip_timers: {ip_timers}")
@@ -53,18 +116,38 @@ def update_timers():
 
 
 def open_port(ip, port):
+    """
+    Opens a specified port for a given IP address using iptables.
+
+    This function checks if an ACCEPT rule for a specific IP and port combination
+    already exists in iptables. If not, it adds this rule. It uses iptables with
+    the mangle table for managing these rules. The function also logs the action
+    taken and updates the `ip_timers` dictionary to keep track of the time when
+    the port was opened for the given IP.
+
+    Parameters:
+    ip (str): The IP address for which the port needs to be opened.
+    port (int): The port number to be opened for the given IP address.
+
+    Side Effects:
+        - Executes iptables commands to add ACCEPT rules.
+        - Updates the global `ip_timers` dictionary.
+        - Logs actions to the logger.
+
+    Returns:
+    None
+    """
+    base_cmd = "/sbin/iptables -t mangle"
+    rule_check = (f"{base_cmd} -C PREROUTING -i {iface} -d {iface_ip} "
+                  f"-p tcp --dport {port} -s {ip} -j ACCEPT")
+    rule_add = (f"{base_cmd} -I PREROUTING -i {iface} -d {iface_ip} "
+                f"-p tcp --dport {port} -s {ip} -j ACCEPT")
+
     # Open the port for the given IP if it's not already open
-    if (
-        run_cmd(
-            f"/sbin/iptables -t mangle -C PREROUTING -i {iface} -d {iface_ip} -p tcp --dport {port} -s {ip} -j ACCEPT"
-        )
-        == 0
-    ):
+    if run_cmd(rule_check) == 0:
         logger.debug(f"Port: {port} already open for source IP address: {ip}")
     else:
-        run_cmd(
-            f"/sbin/iptables -t mangle -I PREROUTING -i {iface} -d {iface_ip} -p tcp --dport {port} -s {ip} -j ACCEPT"
-        )
+        run_cmd(rule_add)
         logger.info(f"Port {port} opened for IP: {ip}")
     # Set the timer for this IP
     ip_timers[ip] = {port: time.time()}
@@ -72,30 +155,88 @@ def open_port(ip, port):
 
 
 def process_packet(packet):
+    """
+    Processes a network packet to check for specific TCP flags and takes action.
+
+    This function examines a given network packet to determine if it contains a TCP 
+    layer with specific flags (specifically, the 'SYN' flag, indicated by "S"). If such 
+    a packet is found, and if its destination port is the port we're monitoring 
+    (`port_to_monitor`), it triggers an action to open the port.
+
+    Parameters:
+    packet: A network packet object, expected to contain IP and TCP layers.
+
+    Side Effects:
+        - Calls `open_port` function with the source IP and destination port of the packet
+          if the packet meets the specified criteria.
+
+    Returns:
+    None
+    """
     if packet.haslayer(TCP) and (packet[TCP].flags == "S"):
         src_ip = packet[IP].src
         dst_port = packet[TCP].dport
-        if dst_port in port_to_monitor:
-            open_port(src_ip, dst_port)
+        open_port(src_ip, dst_port)
 
 
 def ensure_drop_rules(port):
-    # Check if the DROP rule for port {port_to_open} exists, and if not, add it
-    if (
-        run_cmd(
-            f"/sbin/iptables -t mangle -C PREROUTING -i {iface} -d {iface_ip} -p tcp --dport {port} -j DROP"
-        )
-        != 0
-    ):
-        run_cmd(
-            f"/sbin/iptables -t mangle -I PREROUTING -i {iface} -d {iface_ip} -p tcp --dport {port} -j DROP"
-        )
+    """
+    Ensures that a DROP rule exists in iptables for a specific port.
+
+    This function checks if an iptables DROP rule exists for the specified port. 
+    If it does not exist, the function adds the rule. It uses the `iptables` 
+    command with the mangle table to manage the rules. It logs information about 
+    whether the rule was added or already existed.
+
+    Parameters:
+    port (int): The port number for which the DROP rule should be checked or added.
+
+    Side Effects:
+        - Executes system commands to interact with iptables.
+        - Logs information about the existence or addition of DROP rules.
+
+    Returns:
+    None
+    """
+    # Common parts of the command
+    iptables_base_cmd = "/sbin/iptables -t mangle"
+    iptables_common_args = f"-i {iface} -d {iface_ip} -p tcp --dport {port}"
+
+    # Check if the DROP rule for port exists, and if not, add it
+    check_cmd = (f"{iptables_base_cmd} -C PREROUTING "
+                 f"{iptables_common_args} -j DROP")
+    add_cmd = (f"{iptables_base_cmd} -I PREROUTING "
+               f"{iptables_common_args} -j DROP")
+
+    if run_cmd(check_cmd) != 0:
+        run_cmd(add_cmd)
         logger.info(f"DROP rule added for port {port}")
     else:
         logger.info(f"DROP rule already existing for port {port}")
 
 
 def run_cmd(cmd):
+    """
+    Executes a system command passed as a string.
+
+    This function takes a command string, splits it into a list of arguments, 
+    and then executes it using `subprocess.run`. It captures both the standard 
+    output and standard error. The function is designed to handle exceptions that 
+    may occur if the command execution fails, logging the event and setting an 
+    appropriate exit code.
+
+    Parameters:
+    cmd (str): The system command to be executed.
+
+    Returns:
+    int: The exit code of the command execution. Returns 0 if the command executes 
+    successfully, and 1 if there is a `CalledProcessError` exception (indicating 
+    that the command failed).
+
+    Side Effects:
+        - Executes a system command.
+        - Logs debug information about the command execution and its outcome.
+    """
     logger.debug(f"Running command: {cmd}")
     # exit_code = subprocess.run(cmd.split(' '), check=True)
     try:
@@ -111,10 +252,9 @@ def run_cmd(cmd):
 
 if __name__ == "__main__":
     # Ensure default behaviour
-    for port in port_to_monitor:
-        ensure_drop_rules(port_to_monitor)
-    for port in port_to_open:
-        ensure_drop_rules(port_to_open)
+    ensure_drop_rules(port_to_monitor)
+    for p in ports_to_open:
+        ensure_drop_rules(p)
     # load old timers
     ip_timers = load_timers()
     # Capturing traffic
